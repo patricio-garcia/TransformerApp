a09a69c2be538438c60a63d1005461c67c322e0c
Patricio García A <pgarcia@serlitoral.cl>
2020-11-30 16:42:29 -0300

    Proyecto con todos los test

--- app/src/test/java/cl/desafiolatam/transformerapp/TransformerPresenterImplTest.java
+++ app/src/test/java/cl/desafiolatam/transformerapp/TransformerPresenterImplTest.java
@@ -13,6 +13,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.TimeZone;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -38,43 +39,110 @@ public class TransformerPresenterImplTest {
     @Test
     public void setDate() {
         //Given -- Dado
-        calendarDate.set(2020,1,28);
+        calendarDate.set(2020,11,28);
 
         //When -- Cuando
+        presenter.setDate(calendarDate.getTimeInMillis());
 
         //Then -- Entonces
-
+        Mockito.verify(view, Mockito.times(1)).showDateResult();
     }
 
     @Test
     public void getStringDate() {
+        //Given -- Dado
+        calendarDate.set(2020,11,28);
+
+        //When -- Cuando
+        presenter.setDate(calendarDate.getTimeInMillis());
+
+        //Then
+        assertEquals(presenter.getStringDate(), "lunes, diciembre 28, 2020");
+
     }
 
     @Test
     public void getDaysOnly() {
+        //Given -- Dado
+        calendarDate.set(2020,11,28);
+
+        //When -- Cuando
+        presenter.setDate(calendarDate.getTimeInMillis());
+
+        //Then
+        assertEquals(presenter.getDaysOnly(), "Día del mes: 28");
     }
 
     @Test
     public void getWeeksOnly() {
+        //Given -- Dado
+        calendarDate.set(2020,10,28);
+
+        //When -- Cuando
+        presenter.setDate(calendarDate.getTimeInMillis());
+
+        //Then
+        assertEquals(presenter.getWeeksOnly(), "Semana del año: 48");
     }
 
     @Test
     public void getTimeStamp() {
+        //Given -- Dado
+        calendarDate.set(2020,10,28, 00, 00, 00);
+
+        //When -- Cuando
+        presenter.setDate(calendarDate.getTimeInMillis());
+
+        //Then
+        assertNotNull(presenter.getTimeStamp());
+        assertEquals(presenter.getTimeStamp().longValue(), 1606521600000L);
     }
 
     @Test
     public void getDateFormatOne() {
+        //Given -- Dado
+        calendarDate.set(2020,10,28);
+
+        //When -- Cuando
+        presenter.setDate(calendarDate.getTimeInMillis());
+
+        //Then
+        assertEquals(presenter.getDateFormatOne(), "28/11/2020");
     }
 
     @Test
     public void getDateFormatTwo() {
+        //Given -- Dado
+        calendarDate.set(2020,10,28);
+
+        //When -- Cuando
+        presenter.setDate(calendarDate.getTimeInMillis());
+
+        //Then
+        assertEquals(presenter.getDateFormatTwo(), "28 - 11 - 2020");
     }
 
     @Test
     public void getDateFormatThree() {
+        //Given -- Dado
+        calendarDate.set(2020,10,28);
+
+        //When -- Cuando
+        presenter.setDate(calendarDate.getTimeInMillis());
+
+        //Then
+        assertEquals(presenter.getDateFormatThree(), "sáb, nov 28, 2020");
     }
 
     @Test
     public void getDateFormatFour() {
+        //Given -- Dado
+        calendarDate.set(2020,10,28);
+
+        //When -- Cuando
+        presenter.setDate(calendarDate.getTimeInMillis());
+
+        //Then
+        assertEquals(presenter.getDateFormatFour(), "sábado 28");
     }
 }
